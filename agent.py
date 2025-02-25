30066dba879cc431c78f926ef4bdaf7909b694b9
Malki Abderrahmane <malki.abdurrahmane@gmail.com>
2025-02-25 21:26:19 +0100

    fix: update the dataframe loader

--- agent.py
+++ agent.py
@@ -2,20 +2,54 @@ import pandas as pd
 from pandasai import SmartDataframe
 from langchain_groq.chat_models import ChatGroq
 from dotenv import load_dotenv
+from glob import glob
 import re
 import os
 
 load_dotenv()
+ROOT_DIR = os.path.dirname(__file__)
 
 llm = ChatGroq(model_name="llama3-70b-8192", api_key=os.environ["GROQ_API_KEY"])
 
-df = pd.read_excel("example/data.xlsx")
+
+def load_dataframe(folder_path: str):
+    all_dfs = []
+
+    folder_path = os.path.join(ROOT_DIR, folder_path)
+    file_patterns = ["*.xls", "*.xlsx", "*.csv", "*.json", "*.xml", "*.html"]
+    files = []
+
+    for pattern in file_patterns:
+        files.extend(glob(os.path.join(folder_path, pattern)))
+
+    for file_path in files:
+        if file_path.endswith((".xls", ".xlsx")):
+            df = pd.read_excel(file_path)
+        elif file_path.endswith(".csv"):
+            df = pd.read_csv(file_path)
+        elif file_path.endswith(".json"):
+            df = pd.read_json(file_path)
+        elif file_path.endswith(".xml"):
+            df = pd.read_xml(file_path)
+        else:
+            continue
+
+        all_dfs.append(df)
+
+    if all_dfs:
+        return pd.concat(all_dfs, ignore_index=True)
+    else:
+        return pd.DataFrame()
+
+
+df = load_dataframe("data")
 sdf = SmartDataframe(df, config={"llm": llm})
 
 
 def chat(prompt: str):
-    """Processes user queries on the SmartDataframe."""
-
+    """
+    Processes user queries on the SmartDataframe.
+    """
     query = re.sub(r"\s+", " ", prompt).strip()
 
     return sdf.chat(query)
--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import gradio as gr
 import pandas as pd
 from agent import chat
+from typing import Literal, TypedDict
 
 
-def chat_with_dataframe(query, history):
+class Message(TypedDict):
+    role: Literal["user", "assistant"]
+    content: str
+
+
+def chat_with_agent(query: str, history: list[Message] = []) -> str:
     """
     Function to handle chat interactions with the SmartDataframe.
     """
@@ -11,7 +17,7 @@ def chat_with_dataframe(query, history):
         # Execute the query on SmartDataframe
         result = chat(query)
 
-        # Handle different result types
+        # TODO: Handle different result types
         if isinstance(result, pd.DataFrame):
             # For dataframe results
             # return history + [[query, result.to_html()]]
@@ -21,18 +27,19 @@ def chat_with_dataframe(query, history):
             # return history + [[query, str(result)]]
             return str(result)
 
+        # TODO: Save the history of the conversation
+
     except Exception as e:
         return f"An error occurred: {e}"
 
 
-# Gradio Interface
-iface = gr.ChatInterface(
+app = gr.ChatInterface(
     type="messages",
-    fn=chat_with_dataframe,
-    title="Chat with Your DataFrame",
-    description="Ask questions about your data!",
+    fn=chat_with_agent,
+    title="Data Analysis Agent",
+    description="Ask questions about your data files!",
     chatbot=gr.Chatbot(height=500, type="messages"),
 )
 
 if __name__ == "__main__":
-    iface.launch()
+    app.launch()
