a2432c72a00b047a806b21009e796c11df72ec6c
Malki Abderrahmane <malki.abdurrahmane@gmail.com>
2025-02-25 18:18:40 +0100

    feat: create the SDF

--- agent.py
+++ agent.py
@@ -1,9 +1,21 @@
-import os
 import pandas as pd
 from pandasai import SmartDataframe
 from langchain_groq.chat_models import ChatGroq
 from dotenv import load_dotenv
+import re
+import os
 
 load_dotenv()
 
 llm = ChatGroq(model_name="llama3-70b-8192", api_key=os.environ["GROQ_API_KEY"])
+
+df = pd.read_excel("example/data.xlsx")
+sdf = SmartDataframe(df, config={"llm": llm})
+
+
+def chat(prompt: str):
+    """Processes user queries on the SmartDataframe."""
+
+    query = re.sub(r"\s+", " ", prompt).strip()
+
+    return sdf.chat(query)
--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 import gradio as gr
+import pandas as pd
+from agent import chat
 
 
-def greet(name):
-    return f"Hello {name}!"
+def chat_with_dataframe(query, history):
+    """
+    Function to handle chat interactions with the SmartDataframe.
+    """
+    try:
+        # Execute the query on SmartDataframe
+        result = chat(query)
+
+        # Handle different result types
+        if isinstance(result, pd.DataFrame):
+            # For dataframe results
+            # return history + [[query, result.to_html()]]
+            return result.to_html()
+        else:
+            # For other results
+            # return history + [[query, str(result)]]
+            return str(result)
+
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch()
+# Gradio Interface
+iface = gr.ChatInterface(
+    type="messages",
+    fn=chat_with_dataframe,
+    title="Chat with Your DataFrame",
+    description="Ask questions about your data!",
+    chatbot=gr.Chatbot(height=500, type="messages"),
+)
+
+if __name__ == "__main__":
+    iface.launch()
