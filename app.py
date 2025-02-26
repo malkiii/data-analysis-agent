3b216a5cddc1c976755e6a1342c7509b243eab08
Malki Abderrahmane <malki.abdurrahmane@gmail.com>
2025-02-26 21:28:22 +0100

    fix: add more features to the chatbot

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import gradio as gr
 import pandas as pd
 from agent import chat
-from typing import Literal, TypedDict
+from base64 import b64encode
+import os
 
 
-class Message(TypedDict):
-    role: Literal["user", "assistant"]
-    content: str
-
-
-def chat_with_agent(query: str, history: list[Message] = []) -> str:
+def chat_with_agent(query: str, history):
     """
     Function to handle chat interactions with the SmartDataframe.
     """
@@ -17,28 +13,43 @@ def chat_with_agent(query: str, history: list[Message] = []) -> str:
         # Execute the query on SmartDataframe
         result = chat(query)
 
-        # TODO: Handle different result types
+        # For dataframe results
         if isinstance(result, pd.DataFrame):
-            # For dataframe results
-            # return history + [[query, result.to_html()]]
             return result.to_html()
+
+        # For image results
+        elif os.path.exists(str(result)):
+            return f'<img src="{image_to_base64(result)}" alt="{result}" />'
+
+        # For other results
         else:
-            # For other results
-            # return history + [[query, str(result)]]
             return str(result)
 
-        # TODO: Save the history of the conversation
-
     except Exception as e:
-        return f"An error occurred: {e}"
+        return f"CHAT_ERROR: {e}"
+
+
+def image_to_base64(image_path):
+    # Read the image file in binary mode
+    with open(image_path, "rb") as img_file:
+        base64_string = b64encode(img_file.read()).decode("utf-8")
+
+    ext = os.path.splitext(image_path)[1].lower()
+    mime_type = f"image/{ext[1:]}"
+
+    return f"data:{mime_type};base64,{base64_string}"
 
 
 app = gr.ChatInterface(
     type="messages",
     fn=chat_with_agent,
-    title="Data Analysis Agent",
-    description="Ask questions about your data files!",
     chatbot=gr.Chatbot(height=500, type="messages"),
+    css_paths=["public/styles.css"],
+    stop_btn=True,
+    editable=True,
+    autoscroll=True,
+    save_history=True,
+    show_progress=False,
 )
 
 if __name__ == "__main__":
